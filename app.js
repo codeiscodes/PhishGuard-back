9518906fd39f56ca2b3bea98a9097f2e15947308
Naman2701B <namanbadlani27@gmail.com>
2025-01-31 11:45:52 +0530

    backend setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const request = require("request-promise");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/checkUrl", async (req, res, next) => {
+  const options = {
+    method: "POST",
+    uri: "http://127.0.0.1:7000/urlChecker",
+    body: req.body.url,
+    json: true,
+    timeout: 10000, // 10 seconds timeout for the request
+  };
+  const response = await request(options);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
