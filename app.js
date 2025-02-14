71286a44df30ab317b87a8586b531c1ef10d18ef
Naman2701B <namanbadlani27@gmail.com>
2025-02-15 03:17:21 +0530

    Updated backend to connect with python

--- app.js
+++ app.js
@@ -11,14 +11,22 @@ app.use(bodyParser.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/checkUrl", async (req, res, next) => {
+app.post("/checkUrl", async (req, res, next) => {
   const options = {
     method: "POST",
     uri: "http://127.0.0.1:7000/urlChecker",
     body: req.body.url,
     json: true,
   };
+  console.log("URL sent for prediction");
   const response = await request(options);
+  if (response.prediction === "1") {
+    console.log("Model predicted safe");
+    res.status(200).json({ status: true });
+  } else {
+    console.log("Model predicted unsafe");
+    res.status(200).json({ status: false });
+  }
 });
 
 app.listen(PORT, () => {
